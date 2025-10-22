368f2e4668d566366ca33efaf331316b745076a4
manivardhanreddy <manivardhanreddy2@gmail.com>
2025-10-22 20:38:03 +0530

    Task-2

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('🚀 Hello from Node.js CI/CD Pipeline!');
+app.get("/", (req, res) => {
+  res.send("Hello from Node.js CI/CD Pipeline!");
 });
 
 const port = process.env.PORT || 3000;
 
-// 🟢 This keeps the server running
+//  This keeps the server running
 app.listen(port, () => {
-  console.log(`✅ Server running on port ${port}`);
+  console.log(` Server running on port ${port}`);
 });
